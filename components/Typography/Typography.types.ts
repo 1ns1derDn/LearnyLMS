50003bea885b1d79d8419cd341016f6fa17b1014
1ns1derDn <serhiienko98@gmail.com>
2024-05-23 16:20:54 +0400

    Added component Typography

--- components/Typography/Typography.types.ts
+++ components/Typography/Typography.types.ts
@@ -1,11 +1,11 @@
 import { ComponentProps, ElementType, ReactNode } from "react";
 
-type VariantTypes = "Headline1" | "Headline2" | "Text1" | "Text2" | "Text3" | "Text4";
+export type VariantTypes = "headline1" | "headline2" | "text1" | "text2" | "text3" | "text4";
 
 export type TypographyOwnProps<E extends ElementType = ElementType> = {
   children?: ReactNode;
   as?: E;
-  variant: VariantTypes;
+  variant?: VariantTypes;
   className?: string;
 };
 
